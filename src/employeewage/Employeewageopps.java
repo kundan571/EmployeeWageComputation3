2a6c9930899d1f085fb12be85c23bf33cdda1db5
kundan571 <kundanchauhan1997@gmail.com>
2023-02-15 21:45:38 +0530

    Calculate Wages till total working hours or days is reached

--- src/employeewage/Employeewageopps.java
+++ src/employeewage/Employeewageopps.java
@@ -1,16 +1,15 @@
 package employeewage;
 
 public class Employeewageopps {
-    static final int IS_FULL_TIME = 1;
-    static final int EMP_RATE_PER_HOUR = 20;
-    static final int IS_PART_TIME = 2;
-    static final int NO_OF_WORKING_DAYS = 20;
-
-    public static int employeeCheck() {
-        int empCheck = (int) Math.floor((Math.random() * 10) % 2);
+     static final int IS_FULL_TIME = 1;
+     static final int EMP_RATE_PER_HOUR = 20;
+     static final int IS_PART_TIME = 2;
+     static final int NO_OF_WORKING_DAYS = 20;
+     static final int NO_OF_WORKING_HOURS = 100;
+     static int employeeCheck(){
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
-
     public int dailyWageOfEmployee() {
         int empHrs = 0;
         int totalHrs = 0;
@@ -30,7 +29,8 @@ public class Employeewageopps {
                 default:
                     empHrs = 0;
             }
-            totalHrs += empHrs;
+            totalHrs+=empHrs;
+            System.out.println("Day: " + totalWorkingDays + " " + "Emp hr: " + totalHrs);
         }
         totalWage = totalHrs * EMP_RATE_PER_HOUR;
         return totalWage;
@@ -39,6 +39,6 @@ public class Employeewageopps {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         Employeewageopps employee = new Employeewageopps();
-        System.out.println("Monthly Employee Wage: " + employee.dailyWageOfEmployee());
+        System.out.println("Total Wage: " + employee.dailyWageOfEmployee());
     }
 }
