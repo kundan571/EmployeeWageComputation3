bfb218d9d7138fd8919984e65791e318f9bffe4c
kundan571 <kundanchauhan1997@gmail.com>
2023-02-15 19:48:18 +0530

    Added: Part time Employee And Wage

--- src/employeewage/Employeewageopps.java
+++ src/employeewage/Employeewageopps.java
@@ -1,22 +1,24 @@
 package employeewage;
 
 public class Employeewageopps {
-     static final int IS_FULL_TIME = 1;
-     static final int EMP_RATE_PER_HOUR = 20;
+     int IS_FULL_TIME = 2;
+     int IS_PART_TIME = 1;
+     int EMP_RATE_PER_HOUR = 20;
     public int employeeCheck(){
-        int empCheck = (int) Math.floor((Math.random() * 10) % 2);
+        int empCheck =  (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
     public int dailyWageOfEmployee(int empCheck){
         int empHrs = 0;
         int totalWage = 0;
-        //checking if full time or not
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+        if (empCheck == IS_PART_TIME){
+            empHrs = 4;
+        }
+        else if (empCheck == IS_FULL_TIME){
+            empHrs = 8;
+        }
+        else {
+            empHrs = 0;
         }
         totalWage = empHrs * EMP_RATE_PER_HOUR;
         return totalWage;
