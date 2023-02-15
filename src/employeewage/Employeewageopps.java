5c4320541798ef1514f56705c54aaf12b8f8b94a
kundan571 <kundanchauhan1997@gmail.com>
2023-02-15 21:18:06 +0530

    Added: Calculated monthly wage

--- src/employeewage/Employeewageopps.java
+++ src/employeewage/Employeewageopps.java
@@ -1,35 +1,44 @@
 package employeewage;
 
 public class Employeewageopps {
-    public static final int IS_FULL_TIME = 1;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int IS_PART_TIME = 2;
-    public int employeeCheck(){
-        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+    static final int IS_FULL_TIME = 1;
+    static final int EMP_RATE_PER_HOUR = 20;
+    static final int IS_PART_TIME = 2;
+    static final int NO_OF_WORKING_DAYS = 20;
+
+    public static int employeeCheck() {
+        int empCheck = (int) Math.floor((Math.random() * 10) % 2);
         return empCheck;
     }
-    public int dailyWageOfEmployee(int empCheck){
+
+    public int dailyWageOfEmployee() {
         int empHrs = 0;
+        int totalHrs = 0;
         int totalWage = 0;
-        switch (empCheck) {
-            //checking if part time or not
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            //checking if full time or not
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalWorkingDays < NO_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            switch (employeeCheck()) {
+                //checking if Part time or not
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                //checking if full time or not
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            totalHrs += empHrs;
         }
-        totalWage = empHrs * EMP_RATE_PER_HOUR;
+        totalWage = totalHrs * EMP_RATE_PER_HOUR;
         return totalWage;
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         Employeewageopps employee = new Employeewageopps();
-        int isPresent = employee.employeeCheck();
-        System.out.println( "Employe Wage: " + employee.dailyWageOfEmployee(isPresent));
+        System.out.println("Monthly Employee Wage: " + employee.dailyWageOfEmployee());
     }
 }
