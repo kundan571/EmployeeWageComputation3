b75530f4f05e156bb79aa2d72f47a1a92a99ce31
kundan571 <kundanchauhan1997@gmail.com>
2023-02-15 15:27:48 +0530

    Added: Check Employee Attendance

--- src/employeewage/Employeewageopps.java
+++ src/employeewage/Employeewageopps.java
@@ -1,8 +1,22 @@
 package employeewage;
 
 public class Employeewageopps {
+    public int employeeCheck(){
+        int IS_FULL_TIME = 1;
+        int empCheck =  (int)Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
+        return empCheck;
 
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Problem");
+        System.out.println("Welcome to Employee Wage Computation");
+        Employeewageopps empCheck = new Employeewageopps();
+        int isPresent = empCheck.employeeCheck();
+
     }
 }
