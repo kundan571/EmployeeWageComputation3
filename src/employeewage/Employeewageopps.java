c496ddef3ab33e5e39fcb3d5f9b06d04e5dee3ac
kundan571 <kundanchauhan1997@gmail.com>
2023-02-15 21:09:36 +0530

    Added: Switch Case implementation

--- src/employeewage/Employeewageopps.java
+++ src/employeewage/Employeewageopps.java
@@ -1,24 +1,27 @@
 package employeewage;
 
 public class Employeewageopps {
-     int IS_FULL_TIME = 2;
-     int IS_PART_TIME = 1;
-     int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_FULL_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_PART_TIME = 2;
     public int employeeCheck(){
-        int empCheck =  (int) Math.floor((Math.random() * 10) % 3);
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
     public int dailyWageOfEmployee(int empCheck){
         int empHrs = 0;
         int totalWage = 0;
-        if (empCheck == IS_PART_TIME){
-            empHrs = 4;
-        }
-        else if (empCheck == IS_FULL_TIME){
-            empHrs = 8;
-        }
-        else {
-            empHrs = 0;
+        switch (empCheck) {
+            //checking if part time or not
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            //checking if full time or not
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
         }
         totalWage = empHrs * EMP_RATE_PER_HOUR;
         return totalWage;
@@ -27,6 +30,6 @@ public class Employeewageopps {
         System.out.println("Welcome to Employee Wage Computation");
         Employeewageopps employee = new Employeewageopps();
         int isPresent = employee.employeeCheck();
-        System.out.println(employee.dailyWageOfEmployee(isPresent));
+        System.out.println( "Employe Wage: " + employee.dailyWageOfEmployee(isPresent));
     }
 }
