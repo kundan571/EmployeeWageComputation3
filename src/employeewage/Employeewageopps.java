7adaf51517fec4c868ea36aef239bb9f4ebacea5
kundan571 <kundanchauhan1997@gmail.com>
2023-02-15 15:25:14 +0530

    Added: Welcome Message

---
+++ src/employeewage/Employeewageopps.java
@@ -0,0 +1,8 @@
+package employeewage;
+
+public class Employeewageopps {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Problem");
+    }
+}
